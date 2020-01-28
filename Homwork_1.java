2ed49fba452acfbc266357bc685512e173dcaa94
Aryana-98 <57937717+Aryana-98@users.noreply.github.com>
2020-01-28 21:38:44 +0300

    Add files via upload

---
+++ Homwork_1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homwork_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dell
+ */
+//public class Homwork_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//  //////////////// example One/////////////////////////
+//
+// String n =JOptionPane.showInputDialog("Enter an Integer");
+//       int num=Integer.parseInt(n);
+//        JOptionPane.showMessageDialog(null, num,"Input",JOptionPane.QUESTION_MESSAGE);
+//       
+//                System.exit(0);
+//   //  //////////////// example Two/////////////////////////
+//
+//JOptionPane.showMessageDialog(null,"the number is even","Message", JOptionPane.INFORMATION_MESSAGE);
+//  //////////////// example Three///////////////////////// 
+//int n = JOptionPane.showConfirmDialog(null,"An Another ?","Selsctin an Opject",JOptionPane.YES_NO_CANCEL_OPTION);
+//
+//        if(true){
+//            JOptionPane.showMessageDialog(null,"Hello");
+//        }
+//        else {
+//            JOptionPane.showMessageDialog(null, "Goodbye");
+//            
+//        } System.exit(0);
+//
+//    }
+// 
+//}
+//   
+//       
